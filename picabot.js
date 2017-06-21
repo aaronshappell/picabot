91952b739d54e0670a4fbcbb1bd377053ed7ed3b
aaronshappell <crumster88@gmail.com>
2017-06-21 18:48:34 +0300

    Add ping command

---
+++ picabot.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const token = "";
+
+var commands = {
+    "ping": {
+        description: "Pings the bot",
+        process: function(message){
+            message.reply("pong");
+        }
+    }
+};
+
+var checkForCommand = function(message){
+    if(!message.author.bot){
+        if(message.content === "!ping"){ //change this
+            commands.ping.process(message);
+        }
+    }
+}
+
+bot.on("ready", () => console.log("Bot ready!"));
+bot.on("guildMemberAdd", member => member.guild.defaultChannel.send(`Welcome to the server, ${member}!`));
+bot.on("message", message => checkForCommand(message));
+bot.on("messageUpdate", (oldMessage, newMessage) => checkForCommand(newMessage));
+
+bot.login(token);
