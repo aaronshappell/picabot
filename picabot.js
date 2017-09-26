e0a4b03891828663987e5ffe7ec0839f71a0e66f
aaronshappell <crumster88@gmail.com>
2017-09-25 18:51:09 -0700

    Change botChannelName

--- picabot.js
+++ picabot.js
@@ -7,7 +7,7 @@ const youtube = google.youtube("v3");
 
 const bot = new Discord.Client();
 const prefix = "!";
-const botChannelName = "bot-commands";
+const botChannelName = "pica-commands";
 var botChannel;
 var fortunes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely of it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 var dispatcher;
